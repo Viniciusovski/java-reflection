c353d95a034f4936f214d561e790c6b012d1b72c
Viniciusovski <vini98.2012@gmail.com>
2026-01-31 19:48:42 -0300

    comparando os campos

--- src/main/java/br/vinicius/refl/Transformator.java
+++ src/main/java/br/vinicius/refl/Transformator.java
@@ -1,24 +1,48 @@
 // Generic Types: https://docs.oracle.com/javase/tutorial/java/generics/types.html
 package br.vinicius.refl;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
 // A classe Transformator foi criada para tentar "transformar" um objeto
 // em outro tipo de objeto, possivelmente sua versão DTO (Data Transfer Object).
 public class Transformator {
 
-    // O método transform é genérico: <I, O> significa que ele pode receber
-    // um tipo de entrada (I) e retornar um tipo de saída (O).
-    // No entanto, neste código ainda não há retorno implementado.
-    public <I, O> transform(I input) throws ClassNotFoundException {
+    public <I, O> O transform(I input) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
 
-        // Aqui obtemos a classe do objeto de entrada.
-        // Exemplo: se input for do tipo Produto, source será Produto.class.
         Class<?> source = input.getClass();
+        Class<?> target = Class.forName(source.getName() + "DTO");
 
-        // A ideia é procurar dinamicamente a classe correspondente ao DTO.
-        // Ele tenta carregar uma classe cujo nome seja o mesmo da classe original
-        // acrescido de "DTO". Exemplo: Produto -> ProdutoDTO.
-        Class<?> target = source.forName(source.getClass() + "DTO");
+//      O método getDeclaredConstructor() é usado para acessar um construtor da classe,
+//      e newInstance() é utilizado para criar a instância, respeitando as práticas de segurança
+//      ao não utilizar o construtor padrão potencialmente acessível por outras vias.
+        O targetClass = (O) target.getDeclaredConstructor().newInstance();
 
-        // Neste ponto, o código não faz nada com 'target'.
+        // Pega os campos das classes
+        Field[] sourceFields = source.getDeclaredFields();
+        Field[] targetFields = target.getDeclaredFields();
+
+        Arrays.stream(sourceFields).forEach(sourceField ->
+                Arrays.stream(targetFields).forEach(targetField -> {
+                    validate(sourceField, targetField);
+                    try {
+                        // - pega o valor do campo no objeto original "(sourceField.get(input)" e coloca no campo correspondente do DTO.
+                        targetField.set(targetClass, sourceField.get(input));
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                }));
+
+        return targetClass;
+    }
+
+    private void validate(Field sourceField, Field targetField) {
+        if (sourceField.getName().equals(targetField.getName())
+                && sourceField.getType().equals(targetField.getType())){
+            // Libera acesso a campos privados
+            sourceField.setAccessible(true);
+            targetField.setAccessible(true);
+        }
     }
 }
